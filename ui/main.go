7fc7f474d71f71fdc88d0a7025ee0856b57482c2
Ethan Burns <burns.ethan@gmail.com>
2013-06-24 16:11:42 -0400

    Add a higher-level user interface package.
    This package sits on top of SDL.  It handles:
    	- running code on the main go routine.
    	- caching images.
    	- panicking on errors (not usually Go-idiomatic, but most are really unhandelable anyway).
    	- polling events and delivering them to window-based channels.
    	- and eventually more (text, audio, etc.).

---
+++ ui/main.go
@@ -0,0 +1,42 @@
+// +build ignore
+
+package main
+
+import (
+	"image/color"
+	"os"
+	"time"
+
+	"github.com/eaburns/sdl2"
+	"github.com/eaburns/sdl2/ui"
+)
+
+const (
+	width   = 640
+	height  = 480
+	imgPath = "gopher.png"
+)
+
+func main() {
+	ui.Hijack(main2, 20*time.Millisecond)
+}
+
+func main2() {
+	win := ui.NewWindow("test", width, height)
+	tick := time.NewTicker(20*time.Millisecond)
+	for {
+		select {
+		case ev := <-win.Events():
+			if w, ok := ev.(*sdl2.WindowEvent); ok && w.Event == sdl2.WindowClose {
+				os.Exit(0)
+			}
+		case <-tick.C:
+			win.Draw(func(w ui.Canvas) {
+				w.SetDrawColor(color.Black)
+				w.Clear()
+				w.DrawPng(imgPath, 0, 0)
+				w.Present()
+			})
+		}
+	}
+}
---
+++ ui/ui.go
@@ -0,0 +1,243 @@
+// Package ui provides a higher-level user-interface API using SDL2.
+package ui
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/eaburns/sdl2"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+var (
+	doChan  = make(chan func(), 1)
+	windows = make(map[sdl2.WindowID]*Window, 1)
+)
+
+// Hijack must be called by the main go routine to hijack it for the user interface;
+// it never returns. The function f is called in a new go routine as the new "main" function,
+// and rate is the rate at which the user interface should poll for events.
+func Hijack(f func(), rate time.Duration) {
+	if err := sdl2.Init(sdl2.Everything); err != nil {
+		panic(err)
+	}
+
+	go f()
+
+	tick := time.NewTicker(rate)
+	for {
+		select {
+		case f := <-doChan:
+			f()
+		case <-tick.C:
+			pollEvents()
+		}
+	}
+}
+
+func do(f func()) {
+	done := make(chan struct{})
+	doChan <- func() {
+		f()
+		done <- struct{}{}
+	}
+	<-done
+}
+
+func pollEvents() {
+	for {
+		ev := sdl2.PollEvent()
+		if ev == nil {
+			break
+		}
+		if e, ok := ev.(interface {
+			WindowID() sdl2.WindowID
+		}); ok {
+			win := windows[e.WindowID()]
+			win.events <- ev
+		}
+	}
+}
+
+// A Window is a single window on the user's graphical interface.
+type Window struct {
+	win    *sdl2.Window
+	rend   *sdl2.Renderer
+	events chan interface{}
+	imgs   map[string]img
+}
+
+type img struct {
+	tex           *sdl2.Texture
+	width, height int
+}
+
+// NewWindow returns a new window.
+func NewWindow(title string, w, h int) *Window {
+	win := &Window{
+		events: make(chan interface{}, 1),
+		imgs:   make(map[string]img),
+	}
+	do(func() {
+		var err error
+		x, y := sdl2.WindowPosUndefined, sdl2.WindowPosUndefined
+		win.win, err = sdl2.CreateWindow(title, x, y, w, h, sdl2.Shown|sdl2.OpenGL)
+		if err != nil {
+			panic(err)
+		}
+
+		win.rend, err = sdl2.CreateRenderer(win.win, -1, sdl2.Accelerated)
+		if err != nil {
+			panic(err)
+		}
+		if err := win.rend.SetDrawBlendMode(sdl2.Blend); err != nil {
+			panic(err)
+		}
+
+		windows[win.win.ID()] = win
+	})
+	return win
+}
+
+// Destroy destroys the window.
+func (win *Window) Destroy() {
+	do(func() {
+		win.rend.Destroy()
+		win.win.Destroy()
+	})
+}
+
+// FlushCache flushes any cached textures on this window.
+func (win *Window) FlushCache() {
+	do(func() {
+		for _, img := range win.imgs {
+			img.tex.Destroy()
+		}
+		win.imgs = make(map[string]img)
+	})
+}
+
+// Events returns the event channel for the window.
+func (win *Window) Events() <-chan interface{} {
+	return win.events
+}
+
+// Draw calls a function running on the main go routine, and passes it a canvas
+// which can draw to the window. The function can use the Canvas's operations
+// as long as  it does not use them in a different go routine than the one in which
+// the function was called by Draw.
+func (win *Window) Draw(f func(win Canvas)) {
+	do(func() { f(Canvas{win}) })
+}
+
+// A Canvas can draw to a window.
+// The drawing operations can only be safely used within the main go routine.
+type Canvas struct {
+	win *Window
+}
+
+// Clear clears the canvas with the drawing color.
+func (canv Canvas) Clear() {
+	must(canv.win.rend.Clear())
+}
+
+// DrawLine draws a line on the canvas.
+func (canv Canvas) DrawLine(x1, y1, x2, y2 int) {
+	must(canv.win.rend.DrawLine(x1, y1, x2, y2))
+}
+
+// DrawLines draws a series of connected lines on the canvas.
+func (canv Canvas) DrawLines(points []image.Point) {
+	must(canv.win.rend.DrawLines(points))
+}
+
+// DrawPoint draws a point on the canvas.
+func (canv Canvas) DrawPoint(x, y int) {
+	must(canv.win.rend.DrawPoint(x, y))
+}
+
+// DrawPoint draws multiple points on the canvas.
+func (canv Canvas) DrawPoints(points []image.Point) {
+	must(canv.win.rend.DrawPoints(points))
+}
+
+// DratRect draws a rectangle on the canvas.
+func (canv Canvas) DrawRect(rect *image.Rectangle) {
+	must(canv.win.rend.DrawRect(rect))
+}
+
+// DrawRects draws some number of rectangles on the canvas.
+func (canv Canvas) DrawRects(rects []image.Rectangle) {
+	must(canv.win.rend.DrawRects(rects))
+}
+
+// FillRect fills a rectangle on the canvas with the drawing color.
+func (canv Canvas) FillRect(rect *image.Rectangle) {
+	must(canv.win.rend.FillRect(rect))
+}
+
+// FillRects fills some number of rectangles on the canvas with the drawing color.
+func (canv Canvas) FillRects(rects []image.Rectangle) {
+	must(canv.win.rend.FillRects(rects))
+}
+
+// Present updates the canvas with drawing performed.
+func (canv Canvas) Present() {
+	canv.win.rend.Present()
+}
+
+// SetDrawColor sets the color used for drawing operations (Rect, Line and Clear).
+func (canv Canvas) SetDrawColor(col color.Color) {
+	must(canv.win.rend.SetDrawColor(col))
+}
+
+// DrawPng draws the image loaded from a PNG file to the canvas.
+// The image is drawn with the upper-left corner located at x, y.
+func (canv Canvas) DrawPng(path string, x, y int) {
+	i, ok := canv.win.imgs[path]
+	if !ok {
+		png := loadPng(path)
+		tex, err := sdl2.CreateTextureFromImage(canv.win.rend, png)
+		if err != nil {
+			panic(err)
+		}
+		if err := tex.SetBlendMode(sdl2.Blend); err != nil {
+			panic(err)
+		}
+		i = img{
+			tex:    tex,
+			width:  png.Bounds().Dx(),
+			height: png.Bounds().Dy(),
+		}
+		canv.win.imgs[path] = i
+	}
+	dst := image.Rect(x, y, x+i.width, y+i.height)
+	must(canv.win.rend.Copy(i.tex, nil, &dst))
+}
+
+func loadPng(path string) *image.NRGBA {
+	r, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	img, err := png.Decode(r)
+	if err != nil {
+		panic(err)
+	}
+	return img.(*image.NRGBA)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
